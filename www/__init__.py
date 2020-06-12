c3ef6f99aad439e5c43878d51cb0311ee1d770f9
z86588 <z86588@126.com>
2020-06-12 14:00:53 +0800

    day 13

---
+++ www/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" a python module """
+
+__author__ = 'Jack Zhang'
+
+__all__ = ['apis', 'app', 'config', 'dbhandler', 'handlers', 'pymonitor', 'webframe', 'webObject']
+
+
